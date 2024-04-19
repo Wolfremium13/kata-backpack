71e032bed53b09a068123f67cd222379de7df5ec
Wolfremium13 <wolfremiuminformatica@gmail.com>
2024-04-19 10:29:26 +0100

    ✅ backpack not allow to store iteams above max capacity

--- kata_backpack/solution/BackPack.cs
+++ kata_backpack/solution/BackPack.cs
@@ -3,9 +3,11 @@ namespace kata_backpack.solution;
 public record BackPack
 {
     public List<Item> Items { get; } = [];
+    private const int MaxCapacity = 8;
 
     public void Store(Item item)
     {
-        Items.Add(item);
+        if (Items.Count < MaxCapacity)
+            Items.Add(item);
     }
 }
--- kata_backpack/tests/BackPackShould.cs
+++ kata_backpack/tests/BackPackShould.cs
@@ -18,4 +18,23 @@ public class BackPackShould
 
         backpack.Items.Should().Contain(phone).And.Contain(laptop);
     }
+    
+    [Fact]
+    public void not_allow_to_store_items_above_max_capacity()
+    {
+        const int maxCapacity = 8;
+        var backpack = new BackPack();
+        var phones = new List<Item>();
+        for (var i = 0; i < maxCapacity + 1; i++)
+        {
+            phones.Add(Item.from("Phone", Category.Unknown));
+        }
+
+        foreach (var phone in phones)
+        {
+            backpack.Store(phone);
+        }
+
+        backpack.Items.Should().HaveCount(maxCapacity).And.Contain(phones);
+    }
 }
