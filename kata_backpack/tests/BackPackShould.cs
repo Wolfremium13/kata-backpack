4efa0b6c087661a1320d3b77a29efc1cef9ad9c0
Wolfremium13 <wolfremiuminformatica@gmail.com>
2024-04-19 10:40:36 +0100

    ♻ extract method

--- kata_backpack/tests/BackPackShould.cs
+++ kata_backpack/tests/BackPackShould.cs
@@ -6,37 +6,44 @@ namespace kata_backpack.tests;
 
 public class BackPackShould
 {
-    private readonly BackPack backpack = new();
-    
+    private readonly BackPack _backpack = new();
+
     [Fact]
     public void allow_to_store_items()
     {
         var phone = Item.From("Phone", Category.Unknown);
         var laptop = Item.From("Laptop", Category.Unknown);
 
-        backpack.Store(phone);
-        backpack.Store(laptop);
+        _backpack.Store(phone);
+        _backpack.Store(laptop);
 
-        backpack.Items.Should().Contain(phone).And.Contain(laptop);
+        _backpack.Items.Should().Contain(phone).And.Contain(laptop);
     }
-    
+
     [Fact]
     public void not_allow_to_store_items_above_max_capacity()
     {
         const int maxCapacity = 8;
-        var phones = new List<Item>();
-        for (var i = 0; i < maxCapacity + 1; i++)
-        {
-            phones.Add(Item.From("Phone", Category.Unknown));
-        }
+        var phones = Repeat(Item.From("Phone", Category.Unknown), maxCapacity + 1);
 
         foreach (var phone in phones)
         {
-            backpack.Store(phone);
+            _backpack.Store(phone);
         }
 
-        backpack.Items.Should()
+        _backpack.Items.Should()
             .HaveCount(maxCapacity)
             .And.OnlyContain(item => phones.Contains(item));
+    }
+
+    private static List<Item> Repeat(Item item, int count)
+    {
+        var items = new List<Item>();
+        for (var i = 0; i < count; i++)
+        {
+            items.Add(item);
+        }
+
+        return items;
     }
 }
