24b35fec142baab7f72919323905e4f4e20d9c95
Wolfremium13 <wolfremiuminformatica@gmail.com>
2024-04-19 10:34:56 +0100

    ♻ extract field

--- kata_backpack/tests/BagShould.cs
+++ kata_backpack/tests/BagShould.cs
@@ -6,53 +6,51 @@ namespace kata_backpack.tests;
 
 public class BagShould
 {
+    private readonly Bag _bag = new(Category.Electronics);
+    
     [Fact]
     public void allow_to_store_items()
     {
-        var bag = new Bag();
-        var phone = Item.From("Phone", Category.Unknown);
-        var laptop = Item.From("Laptop", Category.Unknown);
-
-
-        bag.Store(phone);
-        bag.Store(laptop);
-
-        bag.Items.Should().Contain(phone).And.Contain(laptop);
-    }
-
-    [Fact]
-    public void allow_to_store_items_with_the_same_category()
-    {
-        var bag = new Bag(Category.Electronics);
         var phone = Item.From("Phone", Category.Electronics);
         var laptop = Item.From("Laptop", Category.Electronics);
 
 
-        bag.Store(phone);
-        bag.Store(laptop);
+        _bag.Store(phone);
+        _bag.Store(laptop);
 
-        bag.Items.Should().Contain(phone).And.Contain(laptop);
+        _bag.Items.Should().Contain(phone).And.Contain(laptop);
+    }
+
+    [Fact]
+    public void allow_to_store_items_with_the_same_category()
+    {     
+        var phone = Item.From("Phone", Category.Electronics);
+        var laptop = Item.From("Laptop", Category.Electronics);
+
+
+        _bag.Store(phone);
+        _bag.Store(laptop);
+
+        _bag.Items.Should().Contain(phone).And.Contain(laptop);
     }
 
     [Fact]
     public void not_allow_to_store_items_with_different_category()
-    {
-        var bag = new Bag(Category.Electronics);
+    {     
         var phone = Item.From("Phone", Category.Electronics);
         var laptop = Item.From("Laptop", Category.Unknown);
 
 
-        bag.Store(phone);
-        bag.Store(laptop);
+        _bag.Store(phone);
+        _bag.Store(laptop);
 
-        bag.Items.Should().Contain(phone).And.NotContain(laptop);
+        _bag.Items.Should().Contain(phone).And.NotContain(laptop);
     }
 
     [Fact]
     public void not_allow_to_store_items_above_max_capacity()
     {
-        const int maxCapacity = 4;
-        var bag = new Bag(Category.Electronics);
+        const int maxCapacity = 4;     
 
         var phone = Item.From("Phone", Category.Electronics);
         var laptop = Item.From("Laptop", Category.Electronics);
@@ -60,14 +58,14 @@ public class BagShould
         var camera = Item.From("Camera", Category.Electronics);
         var watch = Item.From("Watch", Category.Electronics);
 
-        bag.Store(phone);
-        bag.Store(laptop);
-        bag.Store(tablet);
-        bag.Store(camera);
-        bag.Store(watch);
+        _bag.Store(phone);
+        _bag.Store(laptop);
+        _bag.Store(tablet);
+        _bag.Store(camera);
+        _bag.Store(watch);
 
 
-        bag.Items.Should()
+        _bag.Items.Should()
             .HaveCount(maxCapacity)
             .And.Contain(phone)
             .And.Contain(laptop)
