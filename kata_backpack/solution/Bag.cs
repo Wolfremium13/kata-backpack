f7fb53b47c7976a497e2dd7b5dda94423633cdc6
Wolfremium13 <wolfremiuminformatica@gmail.com>
2024-04-19 10:21:04 +0100

    ✅ not allow to store iteams above max capacity

--- kata_backpack/solution/Bag.cs
+++ kata_backpack/solution/Bag.cs
@@ -3,10 +3,11 @@ namespace kata_backpack.solution;
 public record Bag(Category Category = Category.Unknown)
 {
     public List<Item> Items { get; } = [];
+    private const int MaxCapacity = 4;
 
     public void Store(Item item)
     {
-        if (item.Category == Category)
+        if (item.Category == Category && Items.Count < MaxCapacity)
         {
             Items.Add(item);
         }
--- kata_backpack/tests/BagShould.cs
+++ kata_backpack/tests/BagShould.cs
@@ -12,39 +12,66 @@ public class BagShould
         var bag = new Bag();
         var phone = Item.from("Phone", Category.Unknown);
         var laptop = Item.from("Laptop", Category.Unknown);
-        
+
 
         bag.Store(phone);
         bag.Store(laptop);
 
         bag.Items.Should().Contain(phone).And.Contain(laptop);
     }
-    
+
     [Fact]
     public void allow_to_store_items_with_the_same_category()
     {
         var bag = new Bag(Category.Electronics);
         var phone = Item.from("Phone", Category.Electronics);
         var laptop = Item.from("Laptop", Category.Electronics);
-        
+
 
         bag.Store(phone);
         bag.Store(laptop);
 
         bag.Items.Should().Contain(phone).And.Contain(laptop);
     }
-    
+
     [Fact]
     public void not_allow_to_store_items_with_different_category()
     {
         var bag = new Bag(Category.Electronics);
         var phone = Item.from("Phone", Category.Electronics);
         var laptop = Item.from("Laptop", Category.Unknown);
-        
+
 
         bag.Store(phone);
         bag.Store(laptop);
 
         bag.Items.Should().Contain(phone).And.NotContain(laptop);
+    }
+
+    [Fact]
+    public void not_allow_to_store_items_above_max_capacity()
+    {
+        const int maxCapacity = 4;
+        var bag = new Bag(Category.Electronics);
+
+        var phone = Item.from("Phone", Category.Electronics);
+        var laptop = Item.from("Laptop", Category.Electronics);
+        var tablet = Item.from("Tablet", Category.Electronics);
+        var camera = Item.from("Camera", Category.Electronics);
+        var watch = Item.from("Watch", Category.Electronics);
+
+        bag.Store(phone);
+        bag.Store(laptop);
+        bag.Store(tablet);
+        bag.Store(camera);
+        bag.Store(watch);
+
+
+        bag.Items.Should()
+            .Contain(phone)
+            .And.Contain(laptop)
+            .And.Contain(tablet)
+            .And.Contain(camera)
+            .And.NotContain(watch);
     }
 }
